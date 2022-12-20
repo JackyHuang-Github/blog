11e84bbd8bf8d64a2f0060e48c226ab75facbc85
jacky58_huang <jacky58.huang@gmail.com>
2022-12-20 19:10:32 +0800

    111/12/20-19:10

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
+// use Tymon\JWTAuth\Contracts\JWTSubject;
+use Laravel\Sanctum\HasApiTokens;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable implements JWTSubject
 {
-    use Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,7 +18,7 @@ class UserFactory extends Factory
      */
     public function definition()
     {
-        User::truncate();
+        // User::truncate();
 
         return [
             'name' => $this->faker->name(),
